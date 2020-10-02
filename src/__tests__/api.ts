2846386f73688cb449303e62219aa969d5ec6f80
Thomas Hunt <thomas.hunt@candide.eu>
2020-10-02 16:23:37 +0100

    Adds nextOpenOn method

--- src/__tests__/api.ts
+++ src/__tests__/api.ts
@@ -203,3 +203,261 @@ describe("isOpenOn", () => {
     })
   })
 })
+
+describe.only("nextOpenOn", () => {
+  context("currently open", () => {
+    it("returns current date", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 5, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 6, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 7, startTime: "10:00", endTime: "14:00"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-02T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-02T12:00:00.000").toISOString())
+    })
+  })
+
+  context("closed today, open tomorrow", () => {
+    it("returns tomorrows date", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 5, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 6, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 7, startTime: "10:00", endTime: "14:00"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-05T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-06T10:00:00.000").toISOString())
+    })
+  })
+
+  context("closed until next Monday (dayOfWeek wraps around)", () => {
+    it("returns next Monday", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-05T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-10T10:00:00.000").toISOString())
+    })
+  })
+
+  context("open earlier today but now closed, opening tomorrow", () => {
+    it("returns tomorrow", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-05T16:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-06T10:00:00.000").toISOString())
+    })
+  })
+
+  context("out of season, season starts this year", () => {
+    it("returns start of next season", () => {
+      const {nextOpenOn} = openingHours([
+        {
+          type: "open",
+          dayOfWeek: 1,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "08-01",
+          endDay: "10-31",
+        },
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-05T16:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-08-03T10:00:00.000").toISOString())
+    })
+  })
+
+  context("outside season, season starts next year", () => {
+    it("returns start of season", () => {
+      const {nextOpenOn} = openingHours([
+        {
+          type: "open",
+          dayOfWeek: 1,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "02-01",
+          endDay: "02-28",
+        },
+        {
+          type: "open",
+          dayOfWeek: 1,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "08-01",
+          endDay: "08-28",
+        },
+      ])
+      expect(
+        nextOpenOn(new Date("2020-09-03T16:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2021-02-03T10:00:00.000").toISOString())
+    })
+  })
+
+  context(
+    "inside season dates, but no matching day of the week left in season",
+    () => {
+      it("returns start of next season", () => {
+        const {nextOpenOn} = openingHours([
+          {
+            type: "open",
+            dayOfWeek: 1,
+            startTime: "10:00",
+            endTime: "14:00",
+            startDay: "02-01",
+            endDay: "02-28",
+          },
+          {
+            type: "open",
+            dayOfWeek: 1,
+            startTime: "10:00",
+            endTime: "14:00",
+            startDay: "08-01",
+            endDay: "08-28",
+          },
+        ])
+        expect(
+          nextOpenOn(new Date("2020-02-26T16:00:00.000"))?.toISOString(),
+        ).to.eq(new Date("2020-08-03T10:00:00.000").toISOString())
+      })
+    },
+  )
+
+  context("within season and within opening hours", () => {
+    it("returns current date", () => {
+      const {nextOpenOn} = openingHours([
+        {
+          type: "open",
+          dayOfWeek: 1,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "02-01",
+          endDay: "02-28",
+        },
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-03T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-03T12:00:00.000").toISOString())
+    })
+  })
+
+  context("within season but closed for today", () => {
+    it("returns the following day", () => {
+      const {nextOpenOn} = openingHours([
+        {
+          type: "open",
+          dayOfWeek: 1,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "02-01",
+          endDay: "02-28",
+        },
+        {
+          type: "open",
+          dayOfWeek: 2,
+          startTime: "10:00",
+          endTime: "14:00",
+          startDay: "02-01",
+          endDay: "02-28",
+        },
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-03T16:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-04T10:00:00.000").toISOString())
+    })
+  })
+
+  context("usually open but not this week", () => {
+    it("returns the day after the closed period ends", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 5, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 6, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 7, startTime: "10:00", endTime: "14:00"},
+        {type: "closed", startDay: "01-27", endDay: "02-02"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-01T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-03T10:00:00.000").toISOString())
+    })
+  })
+  context("usually open but not this week", () => {
+    it("returns the day after the closed period ends", () => {
+      const {nextOpenOn} = openingHours([
+        {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 5, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 6, startTime: "10:00", endTime: "14:00"},
+        {type: "open", dayOfWeek: 7, startTime: "10:00", endTime: "14:00"},
+        {type: "closed", startDay: "01-27", endDay: "02-02"},
+      ])
+      expect(
+        nextOpenOn(new Date("2020-02-01T12:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-02-03T10:00:00.000").toISOString())
+    })
+  })
+
+  context(
+    "usually not open until Wednesday, but there's a public holiday on Monday",
+    () => {
+      it("returns Monday", () => {
+        const {nextOpenOn} = openingHours(
+          [
+            {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+            {
+              type: "publicHoliday",
+              isOpen: true,
+              startTime: "08:00",
+              endTime: "16:00",
+            },
+          ],
+          {publicHolidays: ["2020-01-06"]},
+        )
+
+        expect(
+          nextOpenOn(new Date("2020-01-03T11:00:00.000"))?.toISOString(),
+        ).to.eq(new Date("2020-01-06T08:00:00.000").toISOString())
+      })
+    },
+  )
+
+  context("usually open on Monday, but next Monday is a public holday", () => {
+    it("returns Tuesday", () => {
+      const {nextOpenOn} = openingHours(
+        [
+          {type: "open", dayOfWeek: 1, startTime: "10:00", endTime: "14:00"},
+          {type: "open", dayOfWeek: 2, startTime: "10:00", endTime: "14:00"},
+          {type: "open", dayOfWeek: 3, startTime: "10:00", endTime: "14:00"},
+          {type: "open", dayOfWeek: 4, startTime: "10:00", endTime: "14:00"},
+          {type: "open", dayOfWeek: 5, startTime: "10:00", endTime: "14:00"},
+          {
+            type: "publicHoliday",
+            isOpen: false,
+          },
+        ],
+        {publicHolidays: ["2020-01-06"]},
+      )
+
+      expect(
+        nextOpenOn(new Date("2020-01-03T17:00:00.000"))?.toISOString(),
+      ).to.eq(new Date("2020-01-07T10:00:00.000").toISOString())
+    })
+  })
+})
--- src/api.ts
+++ src/api.ts
@@ -8,11 +8,23 @@ import {
   OpeningHours,
   ClosedDateSpan,
   isClosedDateSpan,
+  OpenSeasonSpan,
 } from "./types"
-import {getISODay, format} from "date-fns"
+import {
+  getISODay,
+  format,
+  setDay,
+  addWeeks,
+  closestTo,
+  parse as parseDate,
+  addYears,
+} from "date-fns"
 import {expectSingleResult, expectEOF} from "typescript-parsec"
 import {lexer} from "./lexer"
 import {removeDaysOff, parser} from "./parser"
+
+const removeUndefined = <T>(array: Array<T | undefined>): Array<T> =>
+  array.filter((n): n is T => n !== undefined)
 
 export const parse = (pattern: string): Schedule => {
   if (pattern.trim() === "") {
@@ -23,6 +35,54 @@ export const parse = (pattern: string): Schedule => {
     expectSingleResult(expectEOF(parser.parse(lexer.parse(pattern)))),
   )
 }
+
+const withinDays = (span: OpenSpan | ClosedDateSpan, monthAndDay: string) =>
+  (span.startDay !== undefined &&
+    span.endDay !== undefined &&
+    monthAndDay >= span.startDay &&
+    monthAndDay <= span.endDay) ||
+  (span.startDay !== undefined &&
+    span.endDay !== undefined &&
+    span.endDay < span.startDay &&
+    (monthAndDay >= span.startDay || monthAndDay <= span.endDay))
+
+const withinTimes = (span: OpenSpan, hoursAndMinutes: string) =>
+  hoursAndMinutes >= span.startTime && hoursAndMinutes <= span.endTime
+
+const noDaysSpecified = (span: OpenSpan) =>
+  span.startDay === undefined && span.endDay === undefined
+
+const groupSpansByType = (schedule: Schedule) =>
+  schedule.reduce<{
+    daySpans: Array<OpenSpan>
+    seasonSpans: Array<OpenSeasonSpan>
+    closedSpans: Array<ClosedDateSpan>
+    publicHolidays: Array<PublicHoliday>
+  }>(
+    (possibleSpans, span) => {
+      let spanType:
+        | "daySpans"
+        | "seasonSpans"
+        | "closedSpans"
+        | "publicHolidays" = "closedSpans"
+
+      if (isPublicHoliday(span)) {
+        spanType = "publicHolidays"
+      }
+      if (isClosedDateSpan(span)) {
+        spanType = "closedSpans"
+      }
+      if (isOpenSpan(span)) {
+        spanType = noDaysSpecified(span) ? "daySpans" : "seasonSpans"
+      }
+
+      return {
+        ...possibleSpans,
+        [spanType]: [...possibleSpans[spanType], span],
+      }
+    },
+    {daySpans: [], seasonSpans: [], closedSpans: [], publicHolidays: []},
+  )
 
 export const openingHours = (schedule: Schedule, options?: Options) => {
   const openingHours: OpeningHours = {
@@ -75,33 +135,156 @@ export const openingHours = (schedule: Schedule, options?: Options) => {
         return false
       }
 
-      const withinTimes = (span: OpenSpan) =>
-        hoursAndMinutes >= span.startTime && hoursAndMinutes <= span.endTime
-
-      const withinDays = (span: OpenSpan) =>
-        (span.startDay !== undefined &&
-          span.endDay !== undefined &&
-          monthAndDay >= span.startDay &&
-          monthAndDay <= span.endDay) ||
-        (span.startDay !== undefined &&
-          span.endDay !== undefined &&
-          span.endDay < span.startDay &&
-          (monthAndDay >= span.startDay || monthAndDay <= span.endDay))
-
-      const noDaysSpecified = (span: OpenSpan) =>
-        span.startDay === undefined && span.endDay === undefined
-
       if (
         spans.some(
           (span) =>
-            (withinTimes(span) && noDaysSpecified(span)) ||
-            (withinTimes(span) && withinDays(span)),
+            (withinTimes(span, hoursAndMinutes) && noDaysSpecified(span)) ||
+            (withinTimes(span, hoursAndMinutes) &&
+              withinDays(span, monthAndDay)),
         )
       ) {
         return true
       }
 
       return false
+    },
+    nextOpenOn(date) {
+      // Open times are not guaranteed correct if spans contradict each other
+      const hoursAndMinutes = format(date, "HH:mm")
+      const monthAndDay = format(date, "MM-dd")
+      const dayOfWeek = getISODay(date)
+
+      const {
+        daySpans,
+        seasonSpans,
+        closedSpans,
+        publicHolidays,
+      } = groupSpansByType(schedule)
+
+      let potentialDates: Array<Date> = []
+
+      // Consider weekday spans
+      if (daySpans.length > 0) {
+        potentialDates = daySpans.map((daySpan) => {
+          if (
+            daySpan.dayOfWeek === dayOfWeek &&
+            withinTimes(daySpan, hoursAndMinutes)
+          ) {
+            // Potential date is today
+            return date
+          }
+          if (daySpan.dayOfWeek > dayOfWeek) {
+            // Potential date is later this week
+            return parseDate(
+              daySpan.startTime,
+              "HH:mm",
+              setDay(date, daySpan.dayOfWeek),
+            )
+          }
+          // Potential date is next week
+          return parseDate(
+            daySpan.startTime,
+            "HH:mm",
+            addWeeks(setDay(date, daySpan.dayOfWeek), 1),
+          )
+        })
+      }
+
+      // Consider seasonal spans
+      if (seasonSpans.length > 0) {
+        potentialDates = [
+          ...potentialDates,
+          ...removeUndefined(
+            seasonSpans.map((seasonSpan) => {
+              const firstDayOfWeekInSpan = addWeeks(
+                setDay(
+                  parseDate(
+                    `${seasonSpan.startDay} ${seasonSpan.startTime}`,
+                    "MM-dd HH:mm",
+                    new Date(),
+                  ),
+                  seasonSpan.dayOfWeek,
+                ),
+                seasonSpan.dayOfWeek < dayOfWeek ? 1 : 0,
+              )
+
+              if (format(firstDayOfWeekInSpan, "MM-dd") > seasonSpan.endDay) {
+                return undefined
+              }
+
+              if (withinDays(seasonSpan, monthAndDay)) {
+                if (
+                  seasonSpan.dayOfWeek === dayOfWeek &&
+                  withinTimes(seasonSpan, hoursAndMinutes)
+                ) {
+                  // Potential date is today
+                  return date
+                }
+                if (seasonSpan.dayOfWeek > dayOfWeek) {
+                  // Potential date is later this week
+                  const nextDate = setDay(date, seasonSpan.dayOfWeek)
+                  const nextMonthAndDay = format(nextDate, "MM-dd")
+                  if (nextMonthAndDay <= seasonSpan.endDay) {
+                    return parseDate(seasonSpan.startTime, "HH:mm", nextDate)
+                  }
+                }
+                // Potential date is next week
+                const nextDate = addWeeks(date, 1)
+                const nextMonthAndDay = format(nextDate, "MM-dd")
+                if (nextMonthAndDay <= seasonSpan.endDay) {
+                  return parseDate(seasonSpan.startTime, "HH:mm", nextDate)
+                }
+              }
+              if (seasonSpan.startDay > monthAndDay) {
+                // Potential date is the first instance of this day of the week in the season this year
+                return firstDayOfWeekInSpan
+              }
+              // Potential date is the first instance of this day of the week in the season next year
+              return addYears(firstDayOfWeekInSpan, 1)
+            }),
+          ),
+        ]
+      }
+
+      // Consider closed spans
+      potentialDates = potentialDates.filter((potentialDate) => {
+        return (
+          closedSpans.length === 0 ||
+          !closedSpans.some((closedSpan) =>
+            withinDays(closedSpan, format(potentialDate, "MM-dd")),
+          )
+        )
+      })
+
+      // Consider holidays
+      if (options?.publicHolidays !== undefined && publicHolidays.length > 0) {
+        const publicHoliday = publicHolidays[0]
+        if (publicHoliday.isOpen) {
+          potentialDates = [
+            ...potentialDates,
+            ...options.publicHolidays.map((holiday) =>
+              parseDate(
+                `${holiday} ${publicHoliday.startTime}`,
+                "yyyy-MM-dd HH:mm",
+                new Date(),
+              ),
+            ),
+          ]
+        } else {
+          potentialDates = potentialDates.filter((potentialDate) => {
+            return !options.publicHolidays?.some(
+              (publicHolidayDate) =>
+                format(potentialDate, "yyy-MM-dd") === publicHolidayDate,
+            )
+          })
+        }
+      }
+
+      if (potentialDates.length === 0) {
+        return null
+      }
+
+      return closestTo(date, potentialDates)
     },
   }
 
